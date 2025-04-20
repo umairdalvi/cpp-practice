4200093c5a6e525d4dcd24907098fe597047a608
umairdalvi <dev.ud02@proton.me>
2025-04-20 15:51:42 +0530

    completed student management program

--- vectors/vector-object.cpp
+++ vectors/vector-object.cpp
@@ -39,6 +39,16 @@ public:
     {
         return this->studentId;
     }
+
+    void setName(string name)
+    {
+        this->studentName = name;
+    }
+
+    void setAge(int age)
+    {
+        this->studentAge = age;
+    }
 };
 
 class StudentManager
@@ -83,18 +93,24 @@ public:
             cout << "\n--------------------------------------\n";
             cout << "No students to display.";
             cout << "\n--------------------------------------\n\n";
+            return;
         }
-        else
+
+        for (int i = 0; i < students.size(); i++)
         {
-            for (int i = 0; i < students.size(); i++)
-            {
-                students[i].displayInfo();
-            }
+            students[i].displayInfo();
         }
     }
 
     void removeStudent()
     {
+        if (students.empty())
+        {
+            cout << "\n--------------------------------------\n";
+            cout << "No students available to remove.";
+            cout << "\n--------------------------------------\n\n";
+            return;
+        }
         int id;
         cout << "Enter Student ID: ";
         cin >> id;
@@ -128,6 +144,90 @@ public:
         cout << "\n--------------------------------------\n";
         cout << "Student with ID " << id << " not found";
         cout << "\n--------------------------------------\n\n";
+    }
+
+    void updateStudentDetails()
+    {
+        if (students.empty())
+        {
+            cout << "\n--------------------------------------\n";
+            cout << "No students available.";
+            cout << "\n--------------------------------------\n\n";
+            return;
+        }
+
+        int id;
+        bool found;
+        cout << "Enter Student ID: ";
+        cin >> id;
+
+        for (int i = 0; i < students.size(); i++)
+        {
+            if (students[i].getId() == id)
+            {
+                students[i].displayInfo();
+                found = true;
+
+                int choice;
+                do
+                {
+                    cout << endl;
+                    cout << "1. Update Student Name." << endl;
+                    cout << "2. Update Student Age." << endl;
+                    cout << "3. Exit" << endl;
+                    cout << "Enter your choice (1-3): ";
+                    cin >> choice;
+
+                    switch (choice)
+                    {
+                    case 1:
+                    {
+                        cin.ignore();
+
+                        string name;
+                        cout << "Enter new name: ";
+                        getline(cin, name);
+
+                        students[i].setName(name);
+
+                        cout << "\n--------------------------------------\n";
+                        cout << "Name updated successfully.";
+                        cout << "\n--------------------------------------\n";
+
+                        break;
+                    }
+                    case 2:
+                    {
+                        int age;
+                        cout << "Enter new age: ";
+                        cin >> age;
+
+                        students[i].setAge(age);
+
+                        cout << "\n--------------------------------------\n";
+                        cout << "Age updated successfully.";
+                        cout << "\n--------------------------------------\n";
+                        break;
+                    }
+                    case 3:
+                    {
+                        cout << "\n--------------------------------------\n";
+                        cout << "Returning to main menu.";
+                        cout << "\n--------------------------------------\n";
+
+                        break;
+                    }
+                    }
+
+                } while (choice != 3);
+            }
+        }
+        if (!found)
+        {
+            cout << "\n--------------------------------------\n";
+            cout << "Student with ID " << id << " not found.";
+            cout << "\n--------------------------------------\n\n";
+        }
     }
 };
 
@@ -166,6 +266,7 @@ int main()
         }
         case 4:
         {
+            manager.updateStudentDetails();
 
             break;
         }
