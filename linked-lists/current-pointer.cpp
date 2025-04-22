1ad34c3b92790a6d97ecd0632e73026917d0d237
umairdalvi <dev.ud02@proton.me>
2025-04-22 10:38:53 +0530

    current pointer

---
+++ linked-lists/current-pointer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+int main()
+{
+    Node *head = nullptr;
+    head = new Node(10);
+
+    Node *current = new Node(20);
+    head->next = current;
+
+    current = new Node(30);
+    head->next->next = current;
+
+    cout << head->data << endl;
+    cout << head->next->data << endl;
+    cout << head->next->next->data << endl;
+
+    return 0;
+}
