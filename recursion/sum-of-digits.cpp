f43266d2042fd39e47fe2b5dc795c05ca0821a88
umairdalvi <dev.ud02@proton.me>
2025-05-16 12:28:31 +0530

    find sum of digits with recursion

---
+++ recursion/sum-of-digits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    if (n <= 10)
+    {
+        return n;
+    }
+
+    return n % 10 + sumOfDigits(n / 10);
+}
+
+int main()
+{
+
+    int n;
+    cout << "enter a number: ";
+    cin >> n;
+
+    cout << sumOfDigits(n) << endl;
+
+    return 0;
+}
